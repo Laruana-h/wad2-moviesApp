8595c3e1b46ebefc9e6663d68c69a4260be3f59a
sijie he <1274718084@qq.com>
2021-11-08 19:29:27 +0000

    Change upcoming movies page to use caching.

--- src/pages/UpcomingMoviesPage.js
+++ src/pages/UpcomingMoviesPage.js
@@ -3,7 +3,6 @@ import PageTemplate from "../components/templateMovieListPage";
 import { useQuery } from 'react-query'
 import Spinner from '../components/spinner'
 import {getUpcomingMovies} from '../api/tmdb-api'
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
 
 const UpcomingMoviesPage = (props) => {
@@ -19,9 +18,9 @@ return <h1>{error.message}</h1>
 const movies = data.results;
 
 // Redundant, but necessary to avoid app crashing.
-const favorites = movies.filter(m => m.favorite)
-localStorage.setItem('favorites', JSON.stringify(favorites))
-const addToFavorites = (movieId) => true 
+// const favorites = movies.filter(m => m.favorite)
+// localStorage.setItem('favorites', JSON.stringify(favorites))
+
 
 return (
 <PageTemplate
